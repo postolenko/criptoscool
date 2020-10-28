f26d007d497c918dfd409e1ad5979f9c20863f22
postolenko <denis.postolenko@gmail.com>
2020-10-28 18:06:10 +0200

    C

---
+++ js/scripts.js
@@ -0,0 +1,103 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+
+$(window).load(function() {
+
+
+
+});
+
+$(window).resize(function() {
+
+
+
+});
+
+$(document).scroll(function() {
+
+
+
+});
+
+$(document).ready(function() {
+
+    // if( $(".portfolio_slider").length > 0 ) {
+    //     $(".portfolio_slider").not(".slick-initialized").slick({
+    //         dots: true,
+    //         arrows: true,
+    //         autoplay: true,
+    //         autoplaySpeed: 4000,
+    //         speed: 1200,
+    //         slidesToShow: 4,
+    //         slidesToScroll: 1,
+    //         // fade: true,
+    //         responsive: [
+    //             {
+    //               breakpoint: 900,
+    //               settings: {
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 2
+    //               }
+    //             },
+    //             {
+    //               breakpoint: 540,
+    //               settings: {
+    //                 slidesToShow: 1,
+    //                 slidesToScroll: 1
+    //               }
+    //             }
+    //           ]
+    //     });
+    // }
+
+    $(".custom_select_title").on("click", function(e) {
+      e.preventDefault();
+      parentBlock = $(this).closest(".custom_select");
+      dropdownBlock = parentBlock.find(".custom_select_dropdown");
+      if(dropdownBlock.is(":hidden")) {
+        dropdownBlock.slideDown(300);
+        parentBlock.addClass("active");
+      } else {
+        dropdownBlock.slideUp(300);
+        parentBlock.removeClass("active");
+      }
+    });
+
+    $(this).keydown(function(eventObject){
+      if (eventObject.which == 27) {
+          $(".custom_select").each(function() {
+            if($(this).hasClass("active")) {
+              $(this).removeClass("active");
+              $(this).find(".custom_select_dropdown").slideUp(300);
+            }
+          });
+      }
+    });
+
+    $(document).mouseup(function (e){
+        e.preventDefault();
+        var hide_element = $('.custom_select_dropdown');        
+        if (!hide_element.is(e.target)
+                && hide_element.has(e.target).length === 0) {
+                hide_element.closest(".custom_select").removeClass("active");
+                hide_element.slideUp(300);
+        }
+    });
+
+    $(".custom_select_dropdown p").on("click", function(e) {
+      e.preventDefault();
+      value = $(this).text();
+      parentBlock = $(this).closest(".custom_select");
+      inputHidden = parentBlock.find("input[type='hidden']");
+      inputHidden.val(value);
+      selectTitle = parentBlock.find(".custom_select_title p");
+      selectTitle.text(value);
+      $(this).closest(".custom_select_dropdown").slideUp(300);
+      parentBlock.removeClass("active");
+    });
+
+});
